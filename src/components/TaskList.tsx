e65d0bfc2d1ad8d2f19ed59e858d2b1301db5174
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2021-03-26 23:51:50 -0300

    fix(task): create new task

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,11 +15,25 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(), 
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    setTasks(oldValues => [...oldValues, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => task.id === id ? {
+      ...task, 
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
